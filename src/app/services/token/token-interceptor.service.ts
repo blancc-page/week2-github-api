46dbbeca7f418c8a5c8d4e3c93fe504f95d1ddec
blancc-page <blancc.666@gmail.com>
2022-04-18 17:00:57 +0300

    fix[auth]: fix issues with api authorization

--- src/app/services/token/token-interceptor.service.ts
+++ src/app/services/token/token-interceptor.service.ts
@@ -1,21 +1,24 @@
 import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
-  // req!: any;
+  token!: string;
 
-  constructor(private injector: Injector) { }
+  constructor(private injector: Injector) { 
+    const token = environment.API_KEY;
+  }
 
   intercept(req, next){
     let authService = this.injector.get(AuthService)
     let tokenizedReq = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${authService.getToken()}`
+        Authorization: `Bearer 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`
       }
     });
     return next.handle(tokenizedReq);
