54739eab950a646f030fb3737e1d3cbfd246326b
blancc-page <blancc.666@gmail.com>
2022-04-18 16:27:12 +0300

    build[service]: create auth-service

--- src/app/services/token/token-interceptor.service.ts
+++ src/app/services/token/token-interceptor.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http';
+import { AuthService } from '../auth/auth.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -7,12 +9,13 @@ import { HttpInterceptor } from '@angular/common/http';
 export class TokenInterceptorService implements HttpInterceptor {
   // req!: any;
 
-  constructor() { }
+  constructor(private injector: Injector) { }
 
   intercept(req, next){
+    let authService = this.injector.get(AuthService)
     let tokenizedReq = req.clone({
       setHeaders: {
-        Authorization: `Bearer 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`
+        Authorization: `Bearer ${authService.getToken()}`
       }
     });
     return next.handle(tokenizedReq);
